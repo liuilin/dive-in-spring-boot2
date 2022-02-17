637dc0e544d630d6667863ff23fdca4fcff84e6b
liuqiang <liu1142865396@gmail.com>
2022-02-18 00:17:35 +0800

    开启异步 servlet

--- src/main/java/com/liumulin/web/servlet/Servlet.java
+++ src/main/java/com/liumulin/web/servlet/Servlet.java
@@ -1,5 +1,6 @@
 package com.liumulin.web.servlet;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,11 +8,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/my/servlet")
+@WebServlet(urlPatterns = "/my/servlet", asyncSupported = true)
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().write("Hello World");
+        AsyncContext asyncContext = request.startAsync();
+        asyncContext.start(() -> {
+            try {
+                response.getWriter().write("Hello World");
+                // 触发完成
+                asyncContext.complete();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
